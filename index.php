d81c1dda6309f1d74a10299aae901563d16e9cf5
Kirens <eriknygren97@gmail.com>
2014-11-07 17:42:05 +0100

    Initial commit
    Begining of homepage created

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * index.php -- homepage assembled through php
+ *
+ * Copyright (C) 2014 akaProxy
+ *                    Author: Erik Nygren <erik@nygrens.eu>
+ *                            John Daniel Bossér <daniel@bosser.com>
+ *
+ * This file may be used under the terms of the MIT Licence
+ * which can be found in the project root
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hem | Blackebergs elevkår</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <?php require('imports/menu.php'); ?>
+    <div id="container">
+        <header id="slide">
+        </header>
+        <div id="content">
+            <main>
+            </main>
+        </div>
+        
+        <?php require('imports/footer.php'); ?>
+    </div>
+</body>
+</html>
