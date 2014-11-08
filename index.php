c875a5d7f52942369210d946b29a8644733f2e0e
John Daniel <daniel@bosser.com>
2014-11-08 12:16:51 +0100

    fix pointing to right css

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <title>Hem | Blackebergs elevkår</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
     <?php require('imports/menu.php'); ?>
