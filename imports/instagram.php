9626c07354bba81bfb1469a52cbde6ead2706ab9
Kirens <eriknygren97@gmail.com>
2014-11-08 15:41:10 +0100

    Minor adjustments + instagram and menu
    Main structure for content styled

---
+++ imports/instagram.php
@@ -0,0 +1,113 @@
+<div id="ig-container">
+    <ul>
+        <li class="ig-frame-container">
+            <a class="ig-frame" href="http://instagram.com/p/vGNc29ijJk/">
+                <img src="http://instagram.com/p/vGNc29ijJk/media/?size=m">
+                <div class="ig-info">
+                    <p class="description">
+                        Blg älskar kladdkaka 😋💕 #älskakladdkaka #elevkar
+                    </p>
+                    <span class="ig-social">
+                        <span class="ig-likes">
+                            103
+                        </span>
+                        <span class="ig-comments">
+                            0
+                        </span>
+                    </span>
+                </div>
+            </a>
+        </li>
+        <li class="ig-frame-container">
+            <a class="ig-frame" href="http://instagram.com/p/vGHfz9CjDb/">
+                <img src="http://instagram.com/p/vGHfz9CjDb/media/?size=m">
+                <div class="ig-info">
+                    <p class="description">
+                        💕Nu firar vi kladdkakans dag! Kom till ljushal...
+                    </p>
+                    <span class="ig-social">
+                        <span class="ig-likes">
+                            95
+                        </span>
+                        <span class="ig-comments">
+                            1
+                        </span>
+                    </span>
+                </div>
+            </a>
+        </li>
+        <li class="ig-frame-container">
+            <a class="ig-frame" href="http://instagram.com/p/vE24ppijJF/">
+                <img src="http://instagram.com/p/vE24ppijJF/media/?size=m">
+                <div class="ig-info">
+                    <p class="description">
+                        Imorgon bjuder vi på kladdkaka i Ljushallen!
+                    </p>
+                    <span class="ig-social">
+                        <span class="ig-likes">
+                            80
+                        </span>
+                        <span class="ig-comments">
+                            2
+                        </span>
+                    </span>
+                </div>
+            </a>
+        </li>
+        <li class="ig-frame-container">
+            <a class="ig-frame" href="http://instagram.com/p/vBAZYeCjEM/">
+                <img src="http://instagram.com/p/vBAZYeCjEM/media/?size=m">
+                <div class="ig-info">
+                    <p class="description">
+                        🐯Blackans mysigaste etta! 🐯 ✨💫✨🌟Grattis Han...
+                    </p>
+                    <span class="ig-social">
+                        <span class="ig-likes">
+                            41
+                        </span>
+                        <span class="ig-comments">
+                            3
+                        </span>
+                    </span>
+                </div>
+            </a>
+        </li>
+        <li class="ig-frame-container">
+            <a class="ig-frame" href="http://instagram.com/p/u0JzhVijDJ/">
+                <img src="http://instagram.com/p/u0JzhVijDJ/media/?size=m">
+                </img>
+                <div class="ig-info">
+                    <p class="description">
+                        Ettor hör upp!! Den 5 november är det dags för ...
+                    </p>
+                    <span class="ig-social">
+                        <span class="ig-likes">
+                            28
+                        </span>
+                        <span class="ig-comments">
+                            0
+                        </span>
+                    </span>
+                </div>
+            </a>
+        </li>
+        <li class="ig-frame-container">
+            <a class="ig-frame" href="http://instagram.com/p/vGHfz9CjDb/">
+                <img src="http://instagram.com/p/vGHfz9CjDb/media/?size=m">
+                <div class="ig-info">
+                    <p class="description">
+                        💕Nu firar vi kladdkakans dag! Kom till ljushal...
+                    </p>
+                    <span class="ig-social">
+                        <span class="ig-likes">
+                            95
+                        </span>
+                        <span class="ig-comments">
+                            1
+                        </span>
+                    </span>
+                </div>
+            </a>
+        </li>
+    </ul>
+</div>
--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<?php
-/*
- * index.php -- homepage assembled through php
- *
- * Copyright (C) 2014 akaProxy
- *                    Author: Erik Nygren <erik@nygrens.eu>
- *                            John Daniel Bossér <daniel@bosser.com>
- *
- * This file may be used under the terms of the MIT Licence
- * which can be found in the project root
- */
-?>
+<!--
+index.php -- homepage assembled through php
+Copyright (C) 2014 akaProxy
+                   Author: Erik Nygren <erik@nygrens.eu>
+                           John Daniel Bossér <daniel@bosser.com>
+This file may be used under the terms of the MIT Licence
+which can be found in the project root
+-->
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,6 +15,16 @@
     <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
+<!--
+       _            ___                  
+  __ _| |____ _    | _ \_ _ _____ ___  _ 
+ / _` | / / _` |_  |  _/ '_/ _ \ \ / || |
+ \__,_|_\_\__,_(_) |_| |_| \___/_\_\\_, |
+                                    |__/ 
+Det ser ut som Du är en sån som gillar titta under huven, så du är nog en av oss. 
+Joina blackans programmeringsförening idag!
+-->
+
     <?php require('imports/menu.php'); ?>
     <div id="container">
         <header id="slide">
@@ -32,7 +38,7 @@
                     <div class="container">
                         <span class="info">
                             <span class="img-container"><img src="http://lorempixel.com/400/400/people/"/></span>
-                            <h2>Lorem ipsum</h2>
+                            <h2>Dolor sit amet</h2>
                             <p>
                                 <span rel="author">Name Surename</span>
                                 <span>2014-09-03 15:27</span>
@@ -41,27 +47,38 @@
                         <summary>Lorem ipsum dolor sit amet, in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</summary>
                     </div>
                 </div>
-                <form class="card voting">
-                    <!--h2>FRÅGA:</h2-->
-                    <fieldset>
-                        <legend>Vad tycker du om höjden på mjölkborden?</legend>
-                        <p>Mycket klagomål har framförts kring höjden på matborden, nu vill vi veta vad du tycker!</p>
-                        <span class="radio-choice">
-                            <input type="radio" id="radio-btn-1" name="height" value="perfect" checked>
-                              <label for="radio-btn-1">
-                                  <div class="r-btn"></div>
-                                  Det är bra som det är<br></label>
-                            <input type="radio" id="radio-btn-2" name="height" value="too high">
-                              <label for="radio-btn-2">
-                                  <div class="r-btn"></div>
-                                  För höga<br></label>
-                            <input type="radio" id="radio-btn-3" name="height" value="too low">
-                              <label for="radio-btn-3">
-                                  <div class="r-btn"></div>
-                                  För låga</label>
-                        </span>
-                    </fieldset>
-                </form>
+                <div id="external">
+                    <div class="card">
+                        <form class="card voting">
+                            <!--h2>FRÅGA:</h2-->
+                            <fieldset>
+                                <legend>Vad tycker du om höjden på mjölkborden?</legend>
+                                <p>Mycket klagomål har framförts kring höjden på matborden, nu vill vi veta vad du tycker!</p>
+                                <span class="radio-choice">
+                                    <input type="radio" id="radio-btn-1" name="height" value="perfect" checked>
+                                      <label for="radio-btn-1">
+                                          <div class="r-btn"></div>
+                                          Det är bra som det är<br></label>
+                                    <input type="radio" id="radio-btn-2" name="height" value="too high">
+                                      <label for="radio-btn-2">
+                                          <div class="r-btn"></div>
+                                          För höga<br></label>
+                                    <input type="radio" id="radio-btn-3" name="height" value="too low">
+                                      <label for="radio-btn-3">
+                                          <div class="r-btn"></div>
+                                          För låga</label>
+                                </span>
+                            </fieldset>
+                        </form>
+                    </div>
+                    <div class="card">
+                        <h2 class="food">Matsedel:</h2>
+                        <?php require('imports/food.php');?>
+                    </div>
+                    <div class="card">
+                        <?php require('imports/instagram.php'); ?>
+                    </div>
+                </div>
                 <div class="card post">
                     <div class="container">
                         <span class="info">
@@ -79,7 +96,7 @@
                     <div class="container">
                         <span class="info">
                             <span class="img-container"><img src="http://lorempixel.com/402/402/people/"/></span>
-                            <h2>Lorem ipsum</h2>
+                            <h2>Lorem ipsum dolor amet</h2>
                             <p>
                                 <span rel="author">Longname Longsurename</span>
                                 <span>2014-09-03 15:27</span>
@@ -93,6 +110,32 @@
                         <span class="info">
                             <span class="img-container"><img src="http://lorempixel.com/401/401/people/"/></span>
                             <h2>Rorem ipsum</h2>
+                            <p>
+                                <span rel="author">Sh Ort</span>
+                                <span>2014-09-03 15:28</span>
+                            </p>
+                        </span>
+                        <summary>Rorem ipsum dolor sit amet, in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</summary>
+                    </div>
+                </div>
+                <div class="card post">
+                    <div class="container">
+                        <span class="info">
+                            <span class="img-container"><img src="http://lorempixel.com/400/400/people/"/></span>
+                            <h2>Lorem ipsum</h2>
+                            <p>
+                                <span rel="author">Longname Longsurename</span>
+                                <span>2014-09-03 15:27</span>
+                            </p>
+                        </span>
+                        <summary>Lorem ipsum dolor sit amet, in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</summary>
+                    </div>
+                </div>
+                <div class="card post">
+                    <div class="container">
+                        <span class="info">
+                            <span class="img-container"><img src="http://lorempixel.com/398/398/people/"/></span>
+                            <h2>Helg!</h2>
                             <p>
                                 <span rel="author">Sh Ort</span>
                                 <span>2014-09-03 15:28</span>
