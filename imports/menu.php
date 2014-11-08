586eed6bc6f949f80b888c177eca5cd35a4eb2f2
John Daniel <daniel@bosser.com>
2014-11-08 13:26:33 +0100

    progress on both side-menu and menu

--- imports/menu.php
+++ imports/menu.php
@@ -1,4 +1,16 @@
 <div id="menu">
+    <img src="https://www.newfangled.com/wp-content/uploads/2014/08/stuffcontentmgrfiles2ac67e44c30a21635f8a9d498f832bc1cmisc_resized80_313_257_hamenu.png" />
+    <span>BLGELK</span>
 </div>
+
 <div id="side-menu">
+    
+    <img src="img/logo.jpg"/>
+    
+    <ul>
+        <li>Senaste nytt</li>
+        <li>Föreningar</li>
+        <li>Bli medlem</li>
+        <li>Engagera dig</li>
+    </ul>
 </div>
